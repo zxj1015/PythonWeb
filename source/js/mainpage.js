343ac2bf6fc30189d41176610e3f966ba1166d67
zhangxinjian <zhangxinajian1015@gamail.com>
2014-09-25 18:23:24 +0800

    update mainpage

--- source/js/mainpage.js
+++ source/js/mainpage.js
@@ -2,21 +2,51 @@ $(document).ready(function(){
 
 });
 
+function DownLoadFile(fileName){
+   window.open(fileName);
+}
+
 function MenuIsClicked(obj) {
   id = obj.id
+  $("#image_head_page").css("display","none");
+  $("#homepage_news_page").css("display","none");
+  $("#homework_page").css("display","none");
+  $("#book_page").css("display","none");
+  $("#contact_page").css("display","none");
   if (id == "homepage_id") {
-      alert("homepage_id is wrong");
+    $("#image_head_page").css("display","");
+    $("#homepage_news_page").css("display","");	
   } else if (id == "homework_id") {
-      alert("homework_id is wrong");
+    $("#homework_page").css("display","");
   } else if (id == "project_id") {
-      alert("project_id is wrong");
+  
   } else if (id == "software_id") {
-      alert("software_id is wrong");
+
   } else if (id == "book_id") {
-      alert("book_id is wrong");
+    $("#image_head_page").css("display","");
+    $("#book_page").css("display","");
   } else if (id == "contact_id") {
-      alert("concat_id is wrong");
+    $("#image_head_page").css("display",""); 
+    $("#contact_page").css("display","");
   } else {
     alert("something is wrong");
   }
 }
+// select homework date. 
+function SelectHomeWorkWeekNumber(id) {
+  $("#homework_body_page tbody").css("display","none");
+  id=id.split("_")[0];
+  id=id+"_page";
+  $("#"+id).css("display","");
+}
+
+function DownLoadBook(obj) {
+  id = obj.id;
+  DownLoadFile("../../book/python_hexin.pdf");
+}
+
+function DownLoadHomeWork() {
+  alert("Not Finished");
+}
+
+
